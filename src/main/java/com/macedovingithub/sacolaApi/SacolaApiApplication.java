6b307aca7aa2bdbd8c22dc49022fded833340e35
Vinicius Macedo <macedo.vp@gmail.com>
2023-11-17 18:05:05 -0300

    :tada: First commit

---
+++ src/main/java/com/macedovingithub/sacolaApi/SacolaApiApplication.java
@@ -0,0 +1,13 @@
+package com.macedovingithub.sacolaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacolaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacolaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macedovingithub/sacolaApi/SacolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macedovingithub.sacolaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
